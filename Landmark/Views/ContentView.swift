39c9fb3d6529376fc8b9fcfe7fcf91a8860bea4d
adysinghh <aditya12345rajput56@gmail.com>
2024-12-03 16:26:55 +0530

    Added all the Views in ContentView

--- Landmark/Views/ContentView.swift
+++ Landmark/Views/ContentView.swift
@@ -8,8 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    //state variable for the tab selection i.e., featured will be selected as default
+    @State private var selection: Tab = .featured
+    
+    /***
+     enumeration of the tabs to display.
+     */
+    enum Tab {
+            case featured
+            case list
+        }
+    
     var body: some View {
-        LandmarkList()
+        TabView(selection: $selection) {
+            CategoryHome()
+                .tabItem {
+                    Label("Featured", systemImage: "star.fill")
+                }
+                .tag(Tab.featured)
+            
+            LandmarkList()
+                .tabItem {
+                    Label("List", systemImage: "list.bullet")
+                }
+                .tag(Tab.list)
+        }
+//        LandmarkList()
     }
 }
 
