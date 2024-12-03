0ea90962f04550d824fc24b41dea1355d3fc2722
adysinghh <aditya12345rajput56@gmail.com>
2024-12-03 13:00:16 +0530

    Creating a UIPageViewController(Similar to ViewPager in Android)

---
+++ Landmark/PageViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PageViewController.swift
+//  Landmark
+//
+//  Created by Aditya Singh on 03/12/24.
+//
+
+import SwiftUI
+
+struct PageViewController: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PageViewController()
+}
