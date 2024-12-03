5cc6da370cc785f09e7664a76040eeef33b8b440
adysinghh <aditya12345rajput56@gmail.com>
2024-12-03 16:15:08 +0530

    Added renderingMode

--- Landmark/CategoryItem.swift
+++ Landmark/CategoryItem.swift
@@ -5,6 +5,10 @@
 //  Created by Aditya Singh on 03/12/24.
 //
 
+
+/***
+ .renderingMode -> Text that you pass as the label for a navigation link renders using the environment’s accent color, and images may render as template images
+ */
 import SwiftUI
 
 struct CategoryItem: View {
@@ -13,10 +17,12 @@ struct CategoryItem: View {
     var body: some View {
         VStack(alignment: .leading) {
             landmark.image
+                .renderingMode(.original)
                 .resizable()
                 .frame(width: 155,height: 155)
                 .cornerRadius(5)
             Text(landmark.name)
+                .foregroundStyle(.primary)
                 .font(.headline)
         }
     }
