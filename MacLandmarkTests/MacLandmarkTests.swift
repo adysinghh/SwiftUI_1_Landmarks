d75050476442edc5d14abd911cf6b5caf1b20785
Aditya Singh <aditya12345rajput56@gmail.com>
2024-12-28 12:25:28 +0530

    Added MacOS Support

---
+++ MacLandmarkTests/MacLandmarkTests.swift
@@ -0,0 +1,17 @@
+//
+//  MacLandmarkTests.swift
+//  MacLandmarkTests
+//
+//  Created by Aditya Singh on 28/12/24.
+//
+
+import Testing
+@testable import MacLandmark
+
+struct MacLandmarkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ MacLandmarks/MacLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacLandmarksApp.swift
+//  MacLandmarks
+//
+//  Created by Aditya Singh on 28/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MacLandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
